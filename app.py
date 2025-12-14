a2709dd174c250cc6c3b7a6e74458bfd4b6f0cb9
Diasha Ghosh <124531229+G-Diasha@users.noreply.github.com>
2025-12-14 22:21:31 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+import streamlit as st
+from rag_utility import create_vector_db, question_answer
+
+working_dir = os.path.dirname(os.path.abspath((__file__)))
+st.set_page_config(
+    page_title="RAG APP",
+    page_icon="🛠️",
+    layout="centered"
+)
+st.title("🌐 Document Q&A APP (RAG-based)")
+uploaded_file = st.file_uploader("Please upload your PDF file", type=["pdf"])
+
+if uploaded_file is not None:
+    #define the save path
+    save_path = os.path.join(working_dir, uploaded_file.name)
+    #save the file
+    with open(save_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+    process_document = create_vector_db(uploaded_file.name)
+    st.info("Document processed successfully!")
+
+user_question = st.text_area("Ask your question about the document below")
+if st.button("Answer➤"):
+    answer = question_answer(user_question)
+    st.markdown("Response")
+    st.markdown(answer)
---
+++ rag_utility.py
@@ -0,0 +1,54 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import UnstructuredPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+from langchain_groq import ChatGroq
+from langchain_classic.chains import RetrievalQA
+
+load_dotenv()
+
+working_dir = os.path.dirname(os.path.abspath((__file__)))
+
+embed_model = HuggingFaceEmbeddings()
+
+llm= ChatGroq(
+    model = "llama-3.3-70b-versatile",
+    temperature= 0.2
+)
+
+def create_vector_db(filename):
+    loader = UnstructuredPDFLoader(f"{working_dir}\\{filename}")
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 2000,
+        chunk_overlap = 200
+    )
+    text_chunks = text_splitter.split_documents(documents)
+    #Store the document chunks in a Chroma vector database
+    vector_db = Chroma.from_documents(
+        embedding=embed_model,
+        documents= text_chunks,
+        persist_directory= f"{working_dir}\\vector_store_db"
+    )
+    return "Vector store created successfully"
+
+def question_answer(user_question):
+    vector_db = Chroma(
+        embedding_function= embed_model,
+        persist_directory= f"{working_dir}\\vector_store_db"
+    )
+    #Create a retriever for document search
+    retriever = vector_db.as_retriever()
+
+    #Create a RetrievalQA chain to answer user question using Groq model
+
+    qa_chain = RetrievalQA.from_chain_type(
+        llm= llm,
+        retriever = retriever,
+        chain_type = "stuff",
+    )
+    response = qa_chain.invoke({"query": user_question})
+    answer = response["result"]
+    return answer
