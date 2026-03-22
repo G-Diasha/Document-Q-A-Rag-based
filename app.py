f6580de2e13ae01bcc46ca28086c2be925a79383
Diasha Ghosh <124531229+G-Diasha@users.noreply.github.com>
2026-03-22 23:27:29 +0000

    Update app.py

--- app.py
+++ app.py
@@ -15,15 +15,15 @@ st.title("🌐 Document Q&A APP (RAG-based)")
 uploaded_file = st.file_uploader("Please upload your PDF file", type=["pdf"])
 
 if uploaded_file is not None:
-    file_name = os.path.basename(uploaded_file.name)  # ✅ extract clean filename
-    save_path = os.path.join(working_dir, file_name)   # ✅ build full path
+    #file_name = os.path.basename(uploaded_file.name)  # ✅ extract clean filename
+    save_path = os.path.join(working_dir, uploaded_file.name)   # ✅ build full path
     
     with open(save_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
     
-    with st.spinner("Processing document...⏳"):
-        process_document = create_vector_db(uploaded_file.name)  # ✅ pass save_path not uploaded_file.name
+    #with st.spinner("Processing document...⏳"):
     
+    process_document = create_vector_db(uploaded_file.name)  # ✅ pass save_path not uploaded_file.name
     if process_document:
         st.success("Document processed successfully! ✅")
     else:
